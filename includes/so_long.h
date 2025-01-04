d07cdbff7e53ed0ec00f93e9dec7abcd157b2633
Siuol7 <nguyencaotan03072000@gmail.com>
2025-01-04 18:23:14 +0200

    Starting with main and headers

---
+++ includes/so_long.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/04 17:56:04 by caonguye          #+#    #+#             */
+/*   Updated: 2025/01/04 18:15:47 by caonguye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+#include "../library/libft/libft.h"
+#include "../library/ft_printf_fd/ft_printf.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include "MLX42.h"
+
+#endif
---
+++ main/mandatory/main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caonguye <caonguye@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/04 17:55:42 by caonguye          #+#    #+#             */
+/*   Updated: 2025/01/04 18:22:44 by caonguye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <so_long.h>
+
+int	main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		ft_pritnf_fd(2, "Error\n Wrong input!\n Input as follow: ./so_long <map_file>.ber.");
+	}
+	execution(ac, av);
+}
